2eef280a8d95316b252d1235cf0e161e81571319
newcombmc <40343183+newcombmc@users.noreply.github.com>
2019-01-21 15:36:59 -0500

    switched to php 7.1

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 sudo apt update
 sudo apt install python-software-properties
 sudo add-apt-repository ppa:ondrej/php -y
-sudo apt install -y php7.2
-sudo apt install -y php7.2-common php7.2-curl php7.2-gd php7.2-json php7.2-mbstring php7.2-mcrypt 
-sudo apt install -y php7.2-dba php7.2-xml php7.2-zip 
-sudo apt install -y apache2 libapache2-mod-php7.2
-sudo apt install -y mysql-server php7.2-mysql
-sudo mysql_secure_installation
+sudo apt install -y php7.1
+sudo apt install -y php7.1-common php7.1-curl php7.1-gd php7.1-json php7.1-mbstring php7.1-mcrypt 
+sudo apt install -y php7.1-dba php7.1-xml php7.1-zip 
+sudo apt install -y apache2 libapache2-mod-php7.1
+sudo apt install -y mysql-server php7.1-mysql
 sudo apt install unzip
 sudo iptables -A INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT
 sudo iptables -A INPUT -m state --state NEW -p tcp --dport 443 -j ACCEPT
 sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
 sudo service apache2 restart
+sudo a2enmod rewrite
+sudo service apache2 restart
+sudo mysql_secure_installation
