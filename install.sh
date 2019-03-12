ef933bf32b59679b4ef5da705a3fec705a19a31e
newcombmc <40343183+newcombmc@users.noreply.github.com>
2019-03-12 15:55:56 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,17 @@ sudo apt install python-software-properties
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update
 sudo apt install -y php7.1
-sudo apt install -y php7.1-common php7.1-curl php7.1-gd php7.1-json php7.1-mbstring php7.1-mcrypt 
-sudo apt install -y php7.1-dba php7.1-xml php7.1-zip 
-sudo apt install -y apache2 libapache2-mod-php7.1
-sudo apt install -y mysql-server php7.1-mysql
-sudo apt install unzip
+sudo apt install -y php7.1-common 
+sudo apt install -y php7.1-curl 
+sudo apt install -y php7.1-json 
+sudo apt install -y php7.1-mcrypt 
+sudo apt install -y php7.1-dba 
+sudo apt install -y php7.1-zip 
+sudo apt install -y apache2 
+sudo apt install -y 
+sudo apt install -y mysql-server 
+sudo apt install -y
+sudo apt install -y unzip
 sudo iptables -A INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT
 sudo iptables -A INPUT -m state --state NEW -p tcp --dport 443 -j ACCEPT
 sudo ufw allow 80/tcp
@@ -15,4 +21,4 @@ sudo ufw allow 443/tcp
 sudo service apache2 restart
 sudo a2enmod rewrite
 sudo service apache2 restart
-sudo mysql_secure_installation
+
