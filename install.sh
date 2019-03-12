cef312087df99fba6e2acb9393091ce92625556b
newcombmc <40343183+newcombmc@users.noreply.github.com>
2019-03-12 15:57:37 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@ sudo apt install python-software-properties
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update
 sudo apt install -y php7.3
-sudo apt install -y php7.3-common 
-sudo apt install -y php-curl 
-sudo apt install -y php-json 
-sudo apt install -y php-mcrypt 
-sudo apt install -y php-dba 
-sudo apt install -y php-zip 
-sudo apt install -y apache2 
-sudo apt install -y 
-sudo apt install -y mysql-server 
+sudo apt install -y php7.3-common
+sudo apt install -y php-curl
+sudo apt install -y php-json
+sudo apt install -y php-mcrypt
+sudo apt install -y php-dba
+sudo apt install -y php-zip
+sudo apt install -y apache2
+sudo apt install -y
+sudo apt install -y mysql-server
 sudo apt install -y
 sudo apt install -y unzip
 sudo iptables -A INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT
